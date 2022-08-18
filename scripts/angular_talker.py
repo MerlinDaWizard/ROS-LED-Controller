251d85ec318ae74f78a4422c621259b04706d464
Merlin Hancock <merlinhancock@gmail.com>
2022-08-18 16:12:23 +0100

    Huge revamps, more to come? Made listener more oop, with increased performance, now uses a channel system seperated between leds

--- scripts/angular_talker.py
+++ scripts/angular_talker.py
@@ -6,30 +6,18 @@ import rospy
 from std_msgs.msg import String, ColorRGBA
 import geometry_msgs.msg
 from turtlesim.msg import Pose
-from led_locator.msg import LEDdata
-from led_locator.srv import *
+from neopixel_controller.msg import LEDcommand
 import math
 
-def talker():
-    rospy.wait_for_service('get_LED_count')
+def getLEDCount() -> int:
     try:
-        get_LED_count = rospy.ServiceProxy('get_LED_count', GetLEDCount)
-        ledCount = get_LED_count().ledCount
-    except rospy.ServiceException as e:
-        print("Cannot get LED strip length from listener")
-        print(f"Service call failed: {e}")
-    print(f"LED Count={ledCount}")
-    pub = rospy.Publisher('individualLEDset', LEDdata, queue_size=10)
-    rospy.init_node('angularTalker', anonymous=True)
-    rate = rospy.Rate(5)
-
-    while not rospy.is_shutdown():
-        #print(f"Active subscribers = {pub.getActiveSubscribers()}")
-        count = randint(0,ledCount-1)
-        rospy.loginfo(f"Tried setting LED at index {count}")
-        pub.publish(count,(20,0,20))
-        #count = (count + 1) % 10
-        rate.sleep()
+        ledCount = rospy.get_param("neopixel_controller/led_count")
+        print(f"Detected {ledCount} LED(s)")
+    except KeyError as e:
+        print("Could not find parameter for LED count...")
+        print("Assuming LED count 30.")
+        ledCount = 30
+    return ledCount
 
 def thetaToDegrees(theta):
     degrees = theta * (180/math.pi)
@@ -43,41 +31,34 @@ def recievePose(turtlePose):
     global ledCount
     global formerLED
     global pubIndivLED
-    print(turtlePose)
+    #print(turtlePose)
     angle = turtlePose.theta * (180/math.pi)
-    print(f"AnglePre: {angle}")
+    #print(f"AnglePre: {angle}")
     angle = thetaToDegrees(turtlePose.theta) 
-    print(f"AnglePost: {angle}")
+    #print(f"AnglePost: {angle}")
     led = angleToLED(angle,ledCount) # LEDs are 0 based
-    print(f"Publishing to LED {led}")
+    #print(f"Publishing to LED {led}")
     if formerLED == led: # So that we are not constantly publishing the same LED
         return
-    print(formerLED)
-    print(led)
+    #print(formerLED)
+    #print(led)
+    
+    pubIndivLED.publish(led,LEDcommand.GREEN,50)
     if formerLED < ledCount: # Stops a crash with listener on startup due to sneding a publish with >= ledCount, will make fix here and TODO make listener ignore calls that would crash it 
-        pubIndivLED.publish(formerLED,(0,0,0))
-    #fillLED.publish(0,0,0,0)
-    pubIndivLED.publish(led,(20,20,0))
+        pubIndivLED.publish(formerLED,LEDcommand.GREEN,0)
     formerLED = led
+
 def angleToLED(angle, ledCount): # Assuming perfect circle etc etc.
     return int((angle / 360) * (ledCount-1))
     
-def listener():
+def main():
     global ledCount
     global pubIndivLED 
     global fillLED
-    pubIndivLED = rospy.Publisher('individualLEDset', LEDdata, queue_size = 10)  
+    ledCount = getLEDCount()
+    pubIndivLED = rospy.Publisher('multichannel_set_led', LEDcommand, queue_size = 30)  
     #fillLED = rospy.Publisher('fillLEDs', ColorRGBA, queue_size = 10)
     global formerLED
-    #print(rospy.get_param_names())
-    try:
-        ledCount = rospy.get_param("ledlocator/led_count")
-        print(f"Detected {ledCount} LED(s)")
-    except KeyError as e:
-        print("Could not find parameter for LED count...")
-        print("Assuming LED count 30.")
-        ledCount = 30
-        #print("Contacting Service to retrieve it.")
     formerLED = ledCount + 10 # Hopefully always above whatever but keeping it unsigned int
     rospy.init_node('angularLT')
     rospy.Subscriber('turtle1/pose_throttle', Pose, recievePose)
@@ -85,6 +66,6 @@ def listener():
 
 if __name__ == '__main__':
     try:
-        listener()
+        main()
     except rospy.ROSInterruptException:
         pass
--- scripts/filler.py
+++ scripts/filler.py
@@ -4,18 +4,22 @@
 
 import rospy
 from std_msgs.msg import String, ColorRGBA
-from led_locator.msg import LEDdata
+from neopixel_controller.msg import LEDcommand
 
 def filler():
-    pub = rospy.Publisher('fillLEDs', ColorRGBA, queue_size=10)
+    pub = rospy.Publisher('multichannel_set_led', LEDcommand, queue_size=30)
     rospy.init_node('filler', anonymous=True)
     #rate = rospy.Rate(2)
     while not rospy.is_shutdown():
         RGB = input("What colour to fill with? (R,G,B):").split(',')
-        RGB = [int(num) for num in RGB]
-        rospy.loginfo(f"Filling with {RGB} {rospy.get_time()}")
-        pub.publish(RGB[0],RGB[1],RGB[2],0) # Unused Alpha value
-        #count = (count + 1) % 10
+        try:
+            RGB = [int(num) for num in RGB]
+        except ValueError:
+            print("Invalid Number, exitting")
+            exit()
+        rospy.loginfo(f"Filling with {RGB}")
+        for channel, value in enumerate(RGB):
+            pub.publish(LEDcommand.ALLLEDS, channel, value)
 
 if __name__ == '__main__':
     try:
